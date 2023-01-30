c33f14074fb1e928f598af156db299a5aa0727d9
Radhikal0905 <radhikal0905@gmail.com>
2023-01-30 13:11:45 -0600

    testing

---
+++ Practise/src/E2E.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class E2E {
+
+	public static void main(String[] args) {
+		System.setProperty("WebDriver.chrome.driver","C://Program Files (x86)//Google//Chrome//Application//chrome.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+	}
+
+}
